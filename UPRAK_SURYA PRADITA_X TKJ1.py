6f0a53cbb017e8efbefd38476e83dcf326bc47ca
Uyaaa24 <110508765+Uyaaa24@users.noreply.github.com>
2022-08-12 10:38:56 +0700

    Add files via upload

---
+++ UPRAK_SURYA PRADITA_X TKJ1.py
@@ -0,0 +1,11 @@
+# tugas uprak pemrograman dasar
+
+Nama = input("masukkan nama depan anda: ")
+nama = input("masukkan nama belakang anda: ")
+kelas = input("masukkan kelas anda: ")
+print("nama anda adalah", Nama, nama, "dan anda kelas", kelas)
+
+
+suhu = int(input("masukkan suhu celcius: "))
+reamur = (4/5 * suhu) 
+print(suhu, "celcius =", reamur, "reamur")
